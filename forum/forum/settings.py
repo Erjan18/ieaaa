1eb11ab22c9f8796d35dc782d4aa5be0ad09e4d6
Erjan18 <johnydepp2002@mail.ru>
2022-02-12 21:51:37 +0600

    commit

--- forum/forum/settings.py
+++ forum/forum/settings.py
@@ -76,13 +76,9 @@ WSGI_APPLICATION = 'forum.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'db_name',
-        'USER': 'postgres',
-        'PASSWORD': 'qwertij',
-        'HOST': 'localhost',
-        'PORT': '5432',
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
