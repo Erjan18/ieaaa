11f6c0c97488151ce0e265cde70e67e881c6b84b
Erjan18 <johnydepp2002@mail.ru>
2022-02-13 11:43:32 +0600

    commit

--- forum/forum/settings.py
+++ forum/forum/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
 from pathlib import Path
+import django
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -126,3 +128,4 @@ MEDIA_URL = 'media/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
+STATICFILES_ROOT = Path.joinpath(BASE_DIR, "static")
